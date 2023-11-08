ebb68e108e7ea036bcb2904a2535869961007c19
Gan-Ming-Zhong <119864571+Gan-Ming-Zhong@users.noreply.github.com>
2023-11-08 14:49:13 +0800

    Update edatest.py

--- edatest.py
+++ edatest.py
@@ -1,1 +1,9 @@
+import pandas as pd
+import numpy as np
+import seaborn as sns                       #visualisation
+import matplotlib.pyplot as plt             #visualisation
+%matplotlib inline
+sns.set(color_codes=True)
 
+mydf = pd.read_csv("diabetes.csv")
+mydf.head(5)
