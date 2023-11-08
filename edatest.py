b32a41dd662ade98bd586d5bde1810d3ed04262a
Gan-Ming-Zhong <119864571+Gan-Ming-Zhong@users.noreply.github.com>
2023-11-08 14:51:10 +0800

    Update edatest.py

--- edatest.py
+++ edatest.py
@@ -1,9 +1,7 @@
 import pandas as pd
 import numpy as np
-import seaborn as sns                       #visualisation
-import matplotlib.pyplot as plt             #visualisation
-%matplotlib inline
+import seaborn as sns                          
 sns.set(color_codes=True)
-
-mydf = pd.read_csv("diabetes.csv")
-mydf.head(5)
+df = pd.read_csv("diabetes.csv")   
+print(df.tail(5))  
+sns.boxplot(x=df['Age'])
